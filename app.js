86f74ea24db074abaa833115e9b2dcae434fd44f
GothAbby <81208754+GothAbby@users.noreply.github.com>
2021-05-21 00:10:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 function ConvertFahrToCelsius(fahrenheit){
     let celsius = fahrenheit -32 *(5/9);
     return celsius.toFixed(4);
-    
-    
-
 }
 ConvertFahrToCelsius("0");
 ConvertFahrToCelsius(0);
